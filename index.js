9a0acacb3e9f252ed19914b3492900490f86542d
my-rh-r1020 <myrh.comp@gmail.com>
2020-10-15 12:03:50 +0700

    Tambah print Welcome, Menambahkan file index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// File ini untuk Testing GIT Remote
+
+let nilai = prompt("Masukkan Nilai Anda :");
+console.log(nilai);
--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 # File ini untuk Testing GIT Remote
 
+print('WELCOME')
+
 menu_mkn = input('Masukkan Menu Makanan Favorit :')
 print(menu_mkn)
-
 menu_minuman = input('Masukkan Menu Minuman Favorit :')
 print(menu_minuman)
